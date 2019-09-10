44c16082de2e5d639be0ae6984e87fb44d7fb6e8
Cean Manznao <ceanmanzano03@gmail.com>
2019-09-09 18:27:17 -0700

    initial commit for flixster app

---
+++ flixster/flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Cean Manzano on 9/9/19.
+//  Copyright © 2019 Cean Manzano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
